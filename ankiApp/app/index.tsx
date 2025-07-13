93e25b8eb024f6434cac919be1dd2586eb05010e
bnfhayyob <boyabenfath@gmail.com>
2025-07-13 20:07:25 +0200

    setting up tabbar for my home page

---
+++ ankiApp/app/index.tsx
@@ -0,0 +1,30 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Redirect } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {USER_STORAGE_KEY} from '../data/api'
+
+const Page = () => {
+    const [hasId, setHasId] = useState(false)
+
+    useEffect(()=>{
+        const loadId = async () => {
+            const id = await AsyncStorage.getItem(USER_STORAGE_KEY)
+            if(!id){
+              const randomUserid = Math.random().toString(36)
+              console.log(`randomUserid : ${randomUserid}`)
+              await AsyncStorage.setItem(USER_STORAGE_KEY,randomUserid)
+            }
+          setHasId(true)
+        }
+        loadId()
+    },[])
+
+    if(hasId){
+        return <Redirect href="/(tabs)/sets"/>
+    }else{
+        return <View/>
+    }
+}
+
+export default Page
